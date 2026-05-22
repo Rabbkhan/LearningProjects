5ce4da834653083850dc62d83da5802f9bc12737
rabbkhan <rabbilkhan125@gmail.com>
2026-05-23 02:00:33 +0530

    test driven devlopment practice code

--- TDD/app.js
+++ TDD/app.js
@@ -1,6 +1,10 @@
-export function getFullName (fname, lname){
-    return fname + ' ' + lname;
+// export function getFullName (fname, lname){
+//     return fname + ' ' + lname;
+// }
+
+
+
+export function normalizeInput (input){
+    const result = input.trim()
+return result
 }
-
-
-
--- TDD/app.test.js
+++ TDD/app.test.js
@@ -1,11 +1,24 @@
 import test from  'node:test'
-import {getFullName} from './app.js'
+// import {getFullName} from './app.js'
 import  assert  from 'node:assert'
+import { normalizeInput } from './app.js'
 
 
-test("that return the fullname", (t)=>{
+// test("that return the fullname", (t)=>{
 
-    const result = getFullName('Rabbil', 'Khan')
+//     const result = getFullName('Rabbil', 'Khan')
+//     const expected = "Rabbil Khan"
+//     assert.strictEqual(result, expected)
+// })
+
+
+
+test ("that it trimes the spaces", ()=>{
+    const result = normalizeInput("   Rabbil Khan   ")
     const expected = "Rabbil Khan"
     assert.strictEqual(result, expected)
-})
+}
+)
+
+
+test("that it returns empty string")
